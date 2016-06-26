2de7aeb4506545597b64bfdfa5ba02beb32f9b63
Guy Brukhis <guy.br7@gmail.com>
2016-06-26 12:03:25 +0300

    in-memory storage - test evenly generating random option

--- test/storage-in-memory.js
+++ test/storage-in-memory.js
@@ -16,7 +16,30 @@ describe('#storage-in-memory', function() {
     }));
   });
 
-  xit('chooses a random option', function(done) {
+  it('chooses a random option', function(done) {
+    // Run 1000 tests, check that the distribution is pretty much equal between the variants
+    app.get('/test1', function (req, res) {
+      const seed = Math.floor(Math.random() * 10000) + 1000;
+      for (let i = seed; i < seed + 1000; i++) {
+        req.split.set_id(i);
+        req.split.start('test1', function() {
+          res.send();
+        });
+      }
+      req.split.results((results) => {
+        for (let r in results.results.test1) {
+          // At least 29% impressions for each variant is considered normal
+          if (results.results.test1[r].impressions < 290) {
+            console.log(JSON.stringify(results));
+            throw new Error('Got uneven distribution');
+          }
+        }
+        done();
+      });
+    });
+    request(app)
+      .get('/test1')
+      .end();
   });
 
   xit('remembers the chosen option between requests', function(done) {
